bb939903ffcf1e987169b54b5bc0850376f291a7
Maxence de Flotte <maxence@deflotte.fr>
2018-02-12 09:39:05 +0100

    Add env var

--- generate-psmodule-archive.sh
+++ generate-psmodule-archive.sh
@@ -3,12 +3,15 @@
 # usage gma <module-name> <tag|branche>
 
 dir=`pwd`
-git clone git@github.com:madef/$1.git .tmp-$1
-cd .tmp-$1
+if [ -z ${GMA_REPO} ]; then repo_dir=$dir; else repo_dir=$GMA_REPO; fi
+git clone git@github.com:madef/$1.git /tmp/.tmp-$1
+cd /tmp/.tmp-$1
 git fetch --all
 git fetch --tags 
 git checkout $2
-git archive --format=zip --prefix=$1/ -9 $2 > $dir/$1-v$2.zip
+git archive --format=zip --prefix=$1/ -9 $2 > $repo_dir/$1-v$2.zip
 
 cd ..
 rm -rf .tmp-$1
+
+cd $dir
